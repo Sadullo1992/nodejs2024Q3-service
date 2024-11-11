2bf64465e293553d13133fcdb804631bd8a7d4e2
Sadullo <sadulloburiyev1992@gmail.com>
2024-11-11 17:15:36 +0500

    feat: add in memory db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { UserModule } from './resources/user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), UserModule, DatabaseModule],
   controllers: [AppController],
 })
 export class AppModule {}
